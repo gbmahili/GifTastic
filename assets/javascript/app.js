b451a67e95a462037e22801b2af30527e249b502
gbmahili <georgesmahili@gmail.com>
2017-12-16 11:36:52 -0500

    Added firebase and saved all animals into the firebase db

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,27 +1,47 @@
-$(document).ready(() => {
+$(document).ready(() => {  
     
-    
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyAK1XlyW-eK16a8tXT1H8Vqz6UDxOEblgs",
+        authDomain: "rutgersfirebaseintro.firebaseapp.com",
+        databaseURL: "https://rutgersfirebaseintro.firebaseio.com",
+        projectId: "rutgersfirebaseintro",
+        storageBucket: "rutgersfirebaseintro.appspot.com",
+        messagingSenderId: "685944157008"
+    };
+    firebase.initializeApp(config);
+    var database = firebase.database();
+    //Get animals from the db
+    // Firebase is always watching for changes to the data.
+    // When changes occurs it will print them to console and html    
+    database.ref().on("value", function (resp) {
+        gbmGifs.buttonsArray = resp.val();
+        //Show the animals from the firebase database
+        populateFireBaseGifs();
 
+    }, function (error) {
+         console.log("There was an error: " + error.code);
+    });
+
+    var populateFireBaseGifs = () => {
+        //Loop through and show them to the client:
+        for (let i = 0; i < gbmGifs.buttonsArray.animals.length; i++) {
+            //Create a span that will hold each animal button
+            var animalName = $("<span class='animal-name'>");
+            //Add a data attribute that will be used to load out the gifs
+            animalName.attr("data", gbmGifs.buttonsArray.animals[i]);
+            //Add the text to the span which corresponds to the array i
+            animalName.text(gbmGifs.buttonsArray.animals[i]);
+            //Show the span to the user
+            $("#animals-buttons").append(animalName);
+        }
+    };
+
+    //Object:
     var gbmGifs = {
-        //Create a buttons array:
-        buttonsArray : ["DOG", "CAT", "RABBIT", "CHICKEN"],
 
-        //populate buttons:
-        populateAnimalButtons : () => {
-
-            //Loop through and show them to the client:
-            for (let i = 0; i < gbmGifs.buttonsArray.length; i++) {
-                //Create a span that will hold each animal button
-                var animalName = $("<span class='animal-name'>");
-                //Add a data attribute that will be used to load out the gifs
-                animalName.attr("data", gbmGifs.buttonsArray[i]);
-                //Add the text to the span which corresponds to the array i
-                animalName.text(gbmGifs.buttonsArray[i]);
-                //Show the span to the user
-                $("#animals-buttons").append(animalName);
-
-            }
-        },
+        //Create a buttons array:  This list is populated from Firebase db
+        buttonsArray  : [],
 
         //When a user clicks the 'Add Animal', they should push what is added to the array
         addAnimal : function()  {
@@ -31,23 +51,38 @@ $(document).ready(() => {
             newAnimal = newAnimal.toUpperCase().trim();
             //Check if a user entered something:
             if (!newAnimal){
-                alert("Enter Animal Text");
-                gbmGifs.populateAnimalButtons();
+                //Show an alert that
+                alert("Please enter the animal's name!");
+                //Get the list of animals from firebase and show them
+                populateFireBaseGifs();
             }else{              
                 //check if that already exist in the array: if it already exisit, the index will not be -1
-                if (gbmGifs.buttonsArray.indexOf(newAnimal) == -1) {
+                if (gbmGifs.buttonsArray.animals.indexOf(newAnimal) == -1) {
                     //If it doesn't exisit in the array, then push it
-                    gbmGifs.buttonsArray.push(newAnimal);
+                    gbmGifs.buttonsArray.animals.push(newAnimal);
+                    //Save the new array in the database:                    
+                    database.ref().set({
+                        animals: gbmGifs.buttonsArray.animals
+                    });
                 }else{
                     //Change the color of the button the user is trying to add again...
                     //TODO:
-                    $("span:eq("+ gbmGifs.buttonsArray.indexOf(newAnimal)+ ")").css("color", "red");
+                    $("span:eq("+ gbmGifs.buttonsArray.animals.indexOf(newAnimal)+ ")").css("color", "red");
                     //For now, alert that the button has already been added and show the index number:
-                    alert("That animal already exist on INDEX: " + gbmGifs.buttonsArray.indexOf(newAnimal));
+                    alert("That animal already exist on INDEX: " + gbmGifs.buttonsArray.animals.indexOf(newAnimal));
+                    //Show the animals from firebase
+                    populateFireBaseGifs();
                 }
-                //Then call the populateAnimalButtons
-                gbmGifs.populateAnimalButtons();
             }
+        },
+
+        //Clear animals
+        clearAnimals : () => {
+            database.ref().set({
+                animals: ["MONKEY"]
+            });
+            //populateFireBaseGifs();
+
         },
 
         //Show gifs when abutton is clicked
@@ -83,10 +118,6 @@ $(document).ready(() => {
 
     };//End of Object
 
-
-    //Call the populateAnimalButtons on page load
-    gbmGifs.populateAnimalButtons();
-
     //Call the addAnimal method on button click:
     $(".add-animal-button").click(function (e){
         e.preventDefault();        
@@ -96,6 +127,30 @@ $(document).ready(() => {
         gbmGifs.addAnimal();        
         //Empty the input field:
         $("#add-animal").val("");
+
+    });
+
+    //Clear animals buttons:
+    $(".clear-animal-button").click(function (e) {
+        e.preventDefault();
+        //First, we empty the buttons div:
+        $("#animals-buttons").empty();
+        //Check if there are more than one button in the div
+        if (gbmGifs.buttonsArray.animals.length > 1){        
+            //Then call the addAnimal() method to add it to the list        
+            if (confirm("Are you sure? Removing all animals will clear the database and leave one animal only. Are you really sure?") == true){
+                gbmGifs.clearAnimals();
+                //Empty the input field:
+                $("#add-animal").val("");
+                $("#animals").empty();
+            }else{
+                populateFireBaseGifs();
+            }        
+        }else{
+            alert("There is only one button, you can only remove buttons when there is more than one");
+            populateFireBaseGifs();
+        }
+        console.log(gbmGifs.buttonsArray.animals.length);
 
     });
 
@@ -120,11 +175,9 @@ $(document).ready(() => {
     });
 
     //Animate Gif on click
-
     $(document).on("click", ".gif", function () {
         // Let's get the state of the clicked gif: the state was added on the fly
         var state = $(this).attr("data-state");
-        console.log("cliked images");
         //If the state is equal to 'still':
         if (state === "still") {
             //Change the image source and use the link in the data-animate attribute.
@@ -137,5 +190,4 @@ $(document).ready(() => {
             $(this).attr("data-state", "still");
         }
     });
-
 });
